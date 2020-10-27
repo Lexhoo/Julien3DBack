38d51a6059e5f1630ba01e5e598c4a8a710547b7
Lexhoo <lexhoo@yahoo.fr>
2020-10-27 16:21:24 +0100

    supp prod prop

---
+++ src/main/java/com/example/Julien3DBack/config/DataSourceConfiguration.java
@@ -0,0 +1,23 @@
+package com.example.Julien3DBack.config;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableJpaAuditing()
+public class DataSourceConfiguration {
+    @Bean
+    public DataSource getDataSource() {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.username(System.getenv("julien3d_db_user"));
+        dataSourceBuilder.password(System.getenv("julien3d_db_password"));
+        dataSourceBuilder.url(System.getenv("julien3d_db_url"));
+
+        return dataSourceBuilder.build();
+    }
+}
+
